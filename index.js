d13d4caeed0180cf8c2d5e8446980b9e14561a8c
ngochieu642 <ngochieu10041997@gmail.com>
2019-04-26 14:13:35 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app     = express();
+var server  = require('http').createServer(app);
+var io      = require('socket.io').listen(server);
+var fs      = require("fs");
+
+server.listen(process.env.PORT || 11);
+console.log("Server is running on port 11");
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+//Variable for getting position from Python Client
+var myLongitude,myLatitude;
+
+//Variable for Android CLient
+var myPosition;
+myPosition={"Latitude":11.23,"Longitude":101.5};
+
+var count =0;
+
+io.sockets.on('connection',function(socket){
+    console.log("Device connected to the server");
+
+    //Timer task
+    var myTimer = setInterval(function(){
+        console.log("\n\n"+count+" Times");
+        console.log("Sending Infomation to stream...");
+        try {
+            if(myPosition!=null)
+            socket.emit('server-send-vehicle-position',myPosition);   
+            console.log('Send Successfully!');
+            count++;
+        } catch (error) {
+            console.log(error);
+        }
+    },5000);
+});
+
